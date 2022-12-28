6e1efd2aec96faf1990773f55a17715b36f2cdae
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2022-12-28 20:53:10 +0100

    seeding database

---
+++ Brief_Prototype_1/database/factories/BriefFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brief>
+ */
+class BriefFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->unique()->word($maxNbChars=10),
+            'description' => $this->faker->text(),
+            'duration' => $this->faker->numberBetween(1, 30)
+        ];
+    }
+}
