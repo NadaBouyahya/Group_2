86e22baf3831f43a522aa876e4fafd197ca83b53
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-01-05 23:57:20 +0000

    .

---
+++ realisation/front-end/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import AddTutor from './components/AddTutor';
+
+function App() {
+  return (
+    <AddTutor/>
+  );
+}
+
+export default App;
---
+++ realisation/front-end/src/components/AddTutor.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState } from "react"
+
+export default function AddTutor() {
+    const [form, setForm] = useState({});
+    
+    const handleChange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        setForm((old)=>{return {...old, [name]: value}});
+    }
+
+    const Submit = (e) => {
+        e.preventDefault();
+        axios.post('http://127.0.0.1:8000/api/tutors', form).then(()=>{
+            alert("added");
+        })
+    }
+
+    return (
+        <form onSubmit={Submit}>
+            <input type="text" name="firstname" onChange={handleChange} value={form.firstname} />
+            <input type="text" name="lastname" onChange={handleChange} value={form.lastname} />
+            <input type="email" name="email" onChange={handleChange} value={form.email} />
+            <input type="submit" name="submit"/>
+        </form>
+    )
+}
+
+// an3adlo markup
+// xmn tari9a li n5dmo biha
+// 
